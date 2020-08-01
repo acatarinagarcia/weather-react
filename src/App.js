4ad598604a9f64e65e22434f42d3857d92589915
acatarinagarcia <65123646+acatarinagarcia@users.noreply.github.com>
2020-08-01 12:41:00 +0100

    Tried to debugg the deploy on netlify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Catarina G!!!</h1>
+        <h1>Hello Catarina Garcia!!!</h1>
       </header>
     </div>
   );
