ae7d13c23a07e2002c360b36cde7fd7e157e0aeb
acatarinagarcia <65123646+acatarinagarcia@users.noreply.github.com>
2020-08-01 12:46:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Catarina Garcia!!!</h1>
+        <h1>Hello, my name is Catarina Garcia!!!</h1>
       </header>
     </div>
   );
