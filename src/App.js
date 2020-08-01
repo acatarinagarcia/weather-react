8430f263bbc8600823bc1cc64af24498ce2c73ed
acatarinagarcia <65123646+acatarinagarcia@users.noreply.github.com>
2020-08-01 12:24:33 +0100

    Experimenting Stuff

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Catarina!!!</h1>
+        <h1>Hello Catarina G!!!</h1>
       </header>
     </div>
   );
